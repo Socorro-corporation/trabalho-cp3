3a6bc4dcd73ffc7ba87ef35dbc065a26460f824e
jliamonteiro <jlmonteiro2355@gmail.com>
2024-05-25 15:42:50 -0300

    adição do function para validar o usuário

--- js/login.js
+++ js/login.js
@@ -1,29 +1,62 @@
 let listaUsuario = [
     {
-      nomeCompleto: "Jose da Silva",
-      emailUsuario: "jo@email.com",
+      nomeCompleto: "Jeferson Braga",
+      emailUsuario: "jf@email.com",
       senhaUsuario: "123456",
     },
     {
-      nomeCompleto: "Joao Antonio",
-      emailUsuario: "an@email.com",
+      nomeCompleto: "Maria Banks",
+      emailUsuario: "mb@email.com",
       senhaUsuario: "123456",
     },
     {
-      nomeCompleto: "Marco Antonio",
-      emailUsuario: "ma@email.com",
+      nomeCompleto: "Juscelino freire",
+      emailUsuario: "ju@email.com",
       senhaUsuario: "123456",
     },
     {
-      nomeCompleto: "Carlos Silva",
-      emailUsuario: "ca@email.com",
+      nomeCompleto: "Victor Estrella",
+      emailUsuario: "vi@email.com",
       senhaUsuario: "123456",
     },
     {
-      nomeCompleto: "Luis Claudio",
-      emailUsuario: "lc@email.com",
+      nomeCompleto: "Enrico Del Guerra",
+      emailUsuario: "ui@email.com",
       senhaUsuario: "123456",
     },
   ];
   
- 
+  function validar(inputEmail, inputSenha) {
+  
+    //Recuperar elemento de msgStatus
+    let msgStatus = document.querySelector(".valida");
+    
+    for (let x = 0; x < listaUsuario.length; x++) {
+        
+        if ((inputEmail.value == listaUsuario[x].emailUsuario) && (inputSenha.value == listaUsuario[x].senhaUsuario)) {
+          //Redirect
+            msgStatus.setAttribute("class","sucesso");
+            msgStatus.innerText = "Login realizado com sucesso!";
+    
+            //Guardando o objeto validado no localStorage:
+            localStorage.setItem("usuario-logado", JSON.stringify(listaUsuario[x]));
+            
+            setTimeout(function(){
+                msgStatus.setAttribute("class","valida");
+                msgStatus.innerText = "";
+                window.location.href = "../sucesso.html";
+            }, 3000);
+            return false;
+        }
+    }
+        msgStatus.setAttribute("class","erro");
+        msgStatus.innerText = "Login ou senha invalidos!";
+        setTimeout(function(){
+            msgStatus.setAttribute("class","valida");
+            msgStatus.innerText = "";
+            window.location.href = "../erro.html";
+        }, 3000);
+        return false;
+    }
+    
+   
